f14067d853751221d5b0ea1727b9d93a88c2a058
Angela <angeladejuan55@gmail.com>
2025-06-28 00:58:32 +0800

    Import Rabbit Class and Carrot Class

--- game.py
+++ game.py
@@ -2,3 +2,5 @@ import tkinter as tk
 import os
 import random 
 from config import window_width, window_height, score_file, difficulty_settings, background_image_path
+from rabbit import Rabbit
+from carrot import Carrot
