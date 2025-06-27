98152cc5ee81d44d3ef7ec912e9fd78ce862d565
Angela <angeladejuan55@gmail.com>
2025-06-28 00:46:45 +0800

    Draw rabbit image on canvas

--- rabbit.py
+++ rabbit.py
@@ -8,3 +8,4 @@ class Rabbit:
         self.y = window_height - player_height - 10
         img = Image.open(rabbit_image_path).convert("RGBA").resize((player_width, player_height))
         self.image = ImageTk.PhotoImage(img) 
+        self.id = self.canvas.create_image(self.x, self.y, image=self.image, anchor='nw')  
