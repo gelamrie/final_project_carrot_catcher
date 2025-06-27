8fc6d513ed3d0011d2784176c5923e2750abb478
Angela <angeladejuan55@gmail.com>
2025-06-27 23:55:07 +0800

    Difficulty levels and their cond=figurations

--- config.py
+++ config.py
@@ -8,3 +8,10 @@ score_file = "highscore.txt"
 rabbit_image_path = r"C:\Users\Angela\Documents\PUP Files\BsCpE\First Year\Second Semester\OOP\final proj\images\my_rabbit.png"
 carrot_image_path = r"C:\Users\Angela\Documents\PUP Files\BsCpE\First Year\Second Semester\OOP\final proj\images\my_carrot.png"
 background_image_path = r"C:\Users\Angela\Documents\PUP Files\BsCpE\First Year\Second Semester\OOP\final proj\images\background.png"
+
+difficulty_settings = {
+    "Easy": {"speed": 3, "interval": 1200},
+    "Medium":{"speed": 5, "interval": 1000},
+    "Hard": {"speed": 7, "interval": 800}
+}
+    
