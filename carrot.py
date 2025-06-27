6a6924a0a7401a595be51af49698c06070bdd4c5
Angela <angeladejuan55@gmail.com>
2025-06-28 00:34:03 +0800

    Call base class constructor with image path

--- carrot.py
+++ carrot.py
@@ -2,4 +2,5 @@ from falling_objects import FallingObject
 from config import carrot_image_path 
 
 class Carrot(FallingObject): 
-    def __init__(self, canvas, x, speed): 
+    def __init__(self, canvas, x, speed):
+        super().__init__(canvas, x, carrot_image_path, speed) 
