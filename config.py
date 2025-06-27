00f6ea57522edcc0aa56cbcaeea6f1cc366969d2
Angela <angeladejuan55@gmail.com>
2025-06-27 23:49:15 +0800

    File path of rabbit image, carrot image and background image

--- config.py
+++ config.py
@@ -4,3 +4,7 @@ player_width = 60
 player_height = 60
 object_size = 40
 score_file = "highscore.txt"
+
+rabbit_image_path = r"C:\Users\Angela\Documents\PUP Files\BsCpE\First Year\Second Semester\OOP\final proj\images\my_rabbit.png"
+carrot_image_path = r"C:\Users\Angela\Documents\PUP Files\BsCpE\First Year\Second Semester\OOP\final proj\images\my_carrot.png"
+background_image_path = r"C:\Users\Angela\Documents\PUP Files\BsCpE\First Year\Second Semester\OOP\final proj\images\background.png"
