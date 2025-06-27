6fe74a0309c118a055671bb36e0cf99d51e72137
Angela <angeladejuan55@gmail.com>
2025-06-28 00:40:31 +0800

    Constructor to initialize rabbit on the canvas

--- rabbit.py
+++ rabbit.py
@@ -2,4 +2,4 @@ from PIL import Image, Imagetk
 from config import rabbit_image_path, window_width, window_height, player_width, player_height
 
 class Rabbit:
-    
+    def __init__(self, canvas): 
